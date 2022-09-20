4103bccdc5117a45b41bd002247984fe7606c7ca
vladji <vladjislav@gmail.com>
2022-09-21 00:13:03 +0300

    test

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -3,16 +3,15 @@ import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 
 import Layout from "../components/Layout";
-import Slider from "../components/Slider";
 import Features from "../components/Features";
 import PreviewCompatibleImage from "../components/PreviewCompatibleImage";
 
 export const IndexPageTemplate = ({
-                                    sliderData, mainpitch, intro
+                                    mainpitch, intro
                                   }) => {
   return (
     <>
-      <Slider sliderData={sliderData} />
+      {/*<Slider sliderData={sliderData} />*/}
       <section className="section">
         <div className="container">
           <h1 className="h1">{mainpitch.title}</h1>
@@ -23,31 +22,12 @@ export const IndexPageTemplate = ({
             <p className="main-text">{mainpitch.description}</p>
           </div>
           <Features header={intro.header} gridItems={intro.blurbs} />
-          {/* <div className="columns">
-                    <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/products">
-                        See all products
-                      </Link>
-                    </div>
-                  </div> */}
-          {/* <div className="column is-12">
-                    <h3 className="has-text-weight-semibold is-size-2">
-                      Latest stories
-                    </h3>
-                    <BlogRoll />
-                    <div className="column is-12 has-text-centered">
-                      <Link className="btn" to="/blog">
-                        Read more
-                      </Link>
-                    </div>
-                  </div> */}
         </div>
       </section>
     </>);
 };
 
 IndexPageTemplate.propTypes = {
-  sliderData: PropTypes.array,
   mainpitch: PropTypes.object,
   intro: PropTypes.shape({
     blurbs: PropTypes.array
@@ -59,7 +39,6 @@ const IndexPage = ({ data }) => {
   return (
     <Layout>
       <IndexPageTemplate
-        sliderData={frontmatter.slider}
         mainpitch={frontmatter.mainpitch}
         intro={frontmatter.intro}
       />
@@ -80,20 +59,7 @@ export const pageQuery = graphql`
   query IndexPageTemplate {
     markdownRemark(frontmatter: { templateKey: { eq: "index-page" } }) {
       frontmatter {
-        slider {
-          image {
-            childImageSharp {
-              gatsbyImageData(quality: 100, layout: FULL_WIDTH)
-            }
-          }
-          text
-        }
         mainpitch {
-          image {
-            childImageSharp {
-              gatsbyImageData(width: 240, quality: 64, layout: CONSTRAINED)
-            }
-          }
           title
           description
         }
